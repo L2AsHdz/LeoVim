a42264e96eb2221577045ac564bc1c0c3a12e956
L2AsHdz <leonidas0197@gmail.com>
2021-11-19 12:57:22 -0600

    Script para crear enlaces simbolicos agregados

---
+++ createSymLinks.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+sudo ln -sv ~/.dotfiles/.tmux.conf ~
+sudo ln -sv ~/.dotfiles/.p10k.zsh ~
+sudo ln -sv ~/.dotfiles/.zshrc ~
+sudo ln -sv ~/.dotfiles/.gitconfig ~
+sudo ln -sv ~/.dotfiles/nvim/init.vim ~/.config/nvim
+
